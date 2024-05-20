018ff27aca1272ec4271e2496620da743aa08c60
William Laverty <wlaverty13@gmail.com>
2024-05-20 15:02:27 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
-# Non OOP
-# Bank Version 1
-# Single Account
-
-accountName = 'Joe'
-accountBalance = 100
-accountPassword = 'soup'
+account_name = 'Joe'
+account_balance = 100
+account_password = 'soup'
 
 while True:
     print('Press b to get the balance')
     print('Press d to make a deposit')
-    print('Press w to make a withdrawl')
-    print('Press s to show the account')    
+    print('Press w to make a withdrawal')
+    print('Press s to show the account')
     print('Press q to quit')
-    
-    action = input("What do you want to do? ")
-    action = action.lower() #
-    action = action[0]
-    
+
+    action = input("What do you want to do? ").lower()[0]
+
+    if action == 'q':
+        break
+
+    user_password = input('Please enter your password: ')
+    if user_password != account_password:
+        print('Incorrect Password')
+        continue
+
     if action == 'b':
-        print('Get Balance:')
-        userPassword = input('Please enter your password: ')
-        if userPassword != accountPassword:
-            print('Incorrect Password')
-        else:
-            print('Your balance is: ', accountBalance)
+        print('Your balance is: $', account_balance)
+    elif action == 'd':
+        amount = float(input('How much do you want to deposit?: '))
+        account_balance += amount
+        print('Your balance is: $', account_balance)
+    elif action == 'w':
+        amount = float(input('How much do you want to withdraw?: '))
+        account_balance -= amount
+        print('Your balance is: $', account_balance)
+    elif action == 's':
+        print(f'Account details:\nName: {account_name}\nBalance: ')
